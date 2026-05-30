7c7b575b327167b9a78059962b806f51e5f45985
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-05-30 21:51:28 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include "bst.h"
+#include <string>
+
+void makeTree(BST<std::string>&, const char*);
+void printFreq(BST<std::string>&);
 
 int main() {
+    BST<std::string> dictionary;
+    makeTree(dictionary, "src/war_peace.txt");
+    printFreq(dictionary);
     return 0;
 }
